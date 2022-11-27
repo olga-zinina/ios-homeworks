197be5e3ad190f4e9fd0552f2a588aec6643ab83
Olga Zinina <zi.87@mail.ru>
2022-11-27 12:52:13 +0300

    добавлен ксиб

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Maksim Lizunkov on 27.11.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
